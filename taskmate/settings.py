7096cb6cde9b7f55f806d8ec1a0611570d28f9a3
chetanpawar19 <57351087+chetanpawar19@users.noreply.github.com>
2019-12-22 20:19:55 +0530

    Update settings.py

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l_i1gnnz$%gu7-@!9lhysr(!s_ok*tdl)&!piagplp86q!ja61'
+SECRET_KEY = 'your secret key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
